79009bb2805d5b62c0f6c9a744dc8c3b2c75c5ab
pedrpan <psoto004@fiu.edu>
2019-10-29 18:02:49 -0400

    Fixed poly. add. op. and added poly. mult. op.

--- xormat.hs
+++ xormat.hs
@@ -5,21 +5,41 @@ data Matrix a = Rows [[a]] | Cols [[a]] deriving (Show,Ord,Eq,Read)
 data Poly a = Coef [a]  deriving (Show,Ord,Eq,Read)
 
 class Numseq a where
-  (!+) ::  a ->  a ->  a
+  (+++) :: a -> a ->  a
+  (!+) ::  a -> a ->  a
+  (!*) ::  a -> a ->  a
 
 instance Num a => Numseq (Matrix a) where
+  (Rows x) +++ (Rows y) = (Rows (x++y))
+  (Cols x) +++ (Cols y) = (Cols (x++y))
   (Rows x) !+ (Rows []) = (Rows x)
   (Rows []) !+ (Rows x) = (Rows x)
-  (Rows (x:xs)) !+ (Rows (y:ys)) = (Rows [zipWith (+) x y]) +++ ((Rows xs) !+ (Rows ys))
+  (Rows (x:xs)) !+ (Rows (y:ys)) = (Rows [zipWith (+) x y]) +++ the_rest
+    where
+      the_rest = ((Rows xs) !+ (Rows ys))
+  (Rows x) !* (Rows y) = (Rows x) !* (r_to_c (Rows y))
+  (Rows x) !* (Cols []) = (Rows [])
+  (Rows []) !* (Cols x) = (Rows [])
+  (Rows (x:xs)) !* (Cols y) = (Rows first_row) +++ ((Rows xs) !* (Cols y))
+    where
+      first_row = [[(foldr (+) 0 (zipWith (*) x z)) | z<-y]]
 
-instance Num a => Numseq (Poly a) where
+instance (Num a,Enum a,Eq a) => Numseq (Poly a) where
+  (Coef x) +++ (Coef y) = (Coef (x++y))
   (Coef x) !+ (Coef []) = (Coef x)
   (Coef []) !+ (Coef x) = (Coef x)
-  (Coef x) !+ (Coef y) = (Coef (zipWith (+) x y))
-
-(+++) :: Matrix a -> Matrix a -> Matrix a
-(Rows x) +++ (Rows y) = (Rows (x++y))
-(Cols x) +++ (Cols y) = (Cols (x++y))
+  (Coef (x:xs)) !+ (Coef (y:ys)) = (Coef [x + y]) +++ ((Coef xs) !+ (Coef ys))
+  (Coef x) !* (Coef []) = (Coef [0])
+  (Coef []) !* (Coef x) = (Coef [0])
+  (Coef x) !* (Coef [0,1]) = (Coef (0:x))
+  (Coef [0,1]) !* (Coef x) = (Coef (0:x))
+  (Coef (a:as)) !* (Coef (b:bs)) = f !+ i !+ o !+ l
+    where
+      f = (Coef [a*b])
+      i = x !* ((Coef [b]) !* (Coef as))
+      o = x !* ((Coef [a]) !* (Coef bs))
+      l = x !* (x !* ((Coef as) !* (Coef bs)))
+      x = Coef [0,1]
 
 (!**) :: Num a => Matrix a -> Matrix a -> a
 (Rows []) !** (Rows []) = 0
@@ -33,11 +53,8 @@ r_to_c (Rows []) = (Cols [])
 r_to_c (Rows ([]:_)) = (Cols [])
 r_to_c (Rows x)  = (Cols [(map head x)]) +++ (r_to_c (Rows (map tail x)))
 
-(!*) :: Num a => Matrix a -> Matrix a -> Matrix a
-(Rows x) !* (Rows y) = (Rows x) !* (r_to_c (Rows y))
-(Rows x) !* (Cols []) = (Rows [])
-(Rows []) !* (Cols x) = (Rows [])
-(Rows (x:xs)) !* (Cols y) = (Rows [[(foldr (+) 0 (zipWith (*) x z)) | z<-y]]) +++ ((Rows xs) !* (Cols y))
+conv :: Num a => Poly a -> Poly a -> a
+conv (Coef x) (Coef y) = (foldr (+) 0 (zipWith (*) x (reverse y)))
 
 bool_to_comm :: Int -> (Int -> (Int -> Int)) -> Matrix Int
 bool_to_comm n f = (Rows [map g [0..n-1] | g <- (map f [0..n-1]) ])
