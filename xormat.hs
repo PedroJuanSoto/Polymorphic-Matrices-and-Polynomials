f4bfff4085a745ddebd3bc003fa352cf3a99b132
pedrpan <psoto004@fiu.edu>
2019-10-29 00:51:29 -0400

    Added a NumSeq class and added a polynomial data type

--- xormat.hs
+++ xormat.hs
@@ -2,14 +2,24 @@ import Data.Bits
 
 data Matrix a = Rows [[a]] | Cols [[a]] deriving (Show,Ord,Eq,Read)
 
+data Poly a = Coef [a]  deriving (Show,Ord,Eq,Read)
+
+class Numseq a where
+  (!+) ::  a ->  a ->  a
+
+instance Num a => Numseq (Matrix a) where
+  (Rows x) !+ (Rows []) = (Rows x)
+  (Rows []) !+ (Rows x) = (Rows x)
+  (Rows (x:xs)) !+ (Rows (y:ys)) = (Rows [zipWith (+) x y]) +++ ((Rows xs) !+ (Rows ys))
+
+instance Num a => Numseq (Poly a) where
+  (Coef x) !+ (Coef []) = (Coef x)
+  (Coef []) !+ (Coef x) = (Coef x)
+  (Coef x) !+ (Coef y) = (Coef (zipWith (+) x y))
+
 (+++) :: Matrix a -> Matrix a -> Matrix a
 (Rows x) +++ (Rows y) = (Rows (x++y))
 (Cols x) +++ (Cols y) = (Cols (x++y))
-
-(!+) :: Num a => Matrix a -> Matrix a -> Matrix a
-(Rows x) !+ (Rows []) = (Rows x)
-(Rows []) !+ (Rows x) = (Rows x)
-(Rows (x:xs)) !+ (Rows (y:ys)) = (Rows [zipWith (+) x y]) +++ ((Rows xs) !+ (Rows ys))
 
 (!**) :: Num a => Matrix a -> Matrix a -> a
 (Rows []) !** (Rows []) = 0
@@ -18,7 +28,7 @@ data Matrix a = Rows [[a]] | Cols [[a]] deriving (Show,Ord,Eq,Read)
 (!***) :: Num a => a -> Matrix a -> Matrix a
 x !*** (Rows rss) = (Rows (map (map (*x)) rss))
 
-r_to_c :: Matrix a -> Matrix a
+r_to_c :: Num a => Matrix a -> Matrix a
 r_to_c (Rows []) = (Cols [])
 r_to_c (Rows ([]:_)) = (Cols [])
 r_to_c (Rows x)  = (Cols [(map head x)]) +++ (r_to_c (Rows (map tail x)))
@@ -28,10 +38,6 @@ r_to_c (Rows x)  = (Cols [(map head x)]) +++ (r_to_c (Rows (map tail x)))
 (Rows x) !* (Cols []) = (Rows [])
 (Rows []) !* (Cols x) = (Rows [])
 (Rows (x:xs)) !* (Cols y) = (Rows [[(foldr (+) 0 (zipWith (*) x z)) | z<-y]]) +++ ((Rows xs) !* (Cols y))
-
-xor :: Matrix a -> Matrix a
-xor (Rows [[a,b],[c,d]]) = (Rows [[a,b,c,d],[c,d,a,b],[b,a,d,c],[d,c,b,a]])
-
 
 bool_to_comm :: Int -> (Int -> (Int -> Int)) -> Matrix Int
 bool_to_comm n f = (Rows [map g [0..n-1] | g <- (map f [0..n-1]) ])
